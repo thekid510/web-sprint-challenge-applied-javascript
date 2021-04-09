6fde2eaa58c5e8b0b57955cb4968d3db92d1a611
thekid510 <76237067+thekid510@users.noreply.github.com>
2021-04-09 16:22:29 -0700

    finished task 6

--- src/components/card.js
+++ src/components/card.js
@@ -1,37 +1,6 @@
 import axios from "axios";
-const Card = (article) => {
-  const {headline, heroImg, writerName} = article;
-  // Building Hierarchy 
-  const root = document.createElement("div");
-  const heading = document.createElement("div");
-  const author = document.createElement("div");
-  const imgContainer = document.createElement("div");
-  const image = document.createElement("img");
-  const span = document.createElement("span");
- // Adding class names
- root.classList.add("card");
- heading.classList.add("headline");
- author.classList.add("author");
- imgContainer.classList.add("img-container")
- // adding image attribute
- image.setAttribute(`src`, heroImg);
- // text content 
- heading.textContent = `${ headline }`;
- span.textContent = `By ${ writerName }`;
-// appending 
-root.appendChild(headline);
-root.appendChild(author);
-imgContainer.appendChild(image);
-author.appendChild(imgContainer);
-author.appendChild(span);
-
-root.addEventListener("click", (event)=>{
-  console.log(heading);
-})
-
-
-  // TASK 5
-  // ---------------------
+// TASK 5
+  // --------------test change-------
   // Implement this function, which should return the markup you see below.
   // It takes as its only argument an "article" object with `headline`, `authorPhoto` and `authorName` properties.
   // The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
@@ -48,10 +17,40 @@ root.addEventListener("click", (event)=>{
   //   </div>
   // </div>
   //
-}
 
-const cardAppender = (selector) => {
-  // TASK 6
+const Card = (article) => {
+  console.log(article);
+  const {headline, authorPhoto, authorName} = article;
+  // Hierarchy 
+  const root = document.createElement("div");
+  const header = document.createElement("div");
+  const author = document.createElement("div");
+  const imageContainer = document.createElement("div");
+  const img = document.createElement("img");
+  const span = document.createElement("span");
+// adding class names
+  root.classList.add("card");
+  header.classList.add("headline");
+  author.classList.add("author");
+  imageContainer.classList.add("img-container");
+// setting image attribute 
+  img.setAttribute(`src`, authorPhoto);
+  header.textContent = `${ headline }`;
+  span.textContent = `By ${ authorName }`;
+// appending 
+  author.appendChild(imageContainer);
+  root.appendChild(header);
+  root.appendChild(author); 
+  author.appendChild(span);
+  imageContainer.appendChild(img);
+  // event listener to console log headliner
+  root.addEventListener('click', (event) => {
+    console.log(header);
+ });
+ return root;
+  
+}
+ // TASK 6
   // ---------------------
   // Implement this function that takes a css selector as its only argument.
   // It should obtain articles from this endpoint: `https://lambda-times-api.herokuapp.com/articles`
@@ -59,6 +58,28 @@ const cardAppender = (selector) => {
   // Create a card from each and every article object in the response, using the Card component.
   // Append each card to the element in the DOM that matches the selector passed to the function.
   //
+const cardAppender = (selector) => {
+  const card = document.querySelector(selector);
+  axios
+  .get("https://lambda-times-api.herokuapp.com/articles")
+  .then((res)=> {
+    const {articles} = res.data;
+    Object.keys(articles).forEach(article => {
+    for(let item of articles[article]){
+      const element = new Card(item);
+        console.log(articles[article]);
+        card.appendChild(element);
+        console.log(card);
+    }
+    
+    })
+    
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+
+ 
 }
 
 export { Card, cardAppender }
